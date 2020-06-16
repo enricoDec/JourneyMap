3ac3104a3902ec0539b5845abf71ddffb6bdec9f
enricoDec <enricodechadarevian@gmail.com>
2020-06-16 22:54:18 +0200

    main app routing

---
+++ JourneyMap/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='JourneyMap_home'),
+    path('login/', views.sign_in, name='JourneyMap_login'),
+    path('register/', views.sign_up, name='JourneyMap_register'),
+    path('contact_us/', views.contact, name='JourneyMap_contact_us'),
+]
