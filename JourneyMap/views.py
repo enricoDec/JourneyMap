8945b58a367c286f74e1e7fcf569c3a41b739382
enricoDec <enricodechadarevian@gmail.com>
2020-06-16 22:53:57 +0200

    Basic app view

--- JourneyMap/views.py
+++ JourneyMap/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
 
-# Create your views here.
+
+def home(request):
+    context = {
+        'title': _('Home')
+    }
+    return render(request, 'JourneyMap/home.html', context)
+
+
+def sign_in(request):
+    context = {
+        'title': _('Sign In')
+    }
+    return render(request, 'JourneyMap/sign_in.html', context)
+
+
+def sign_up(request):
+    context = {
+        'title': _('Sign Up')
+    }
+    return render(request, 'JourneyMap/sign_up.html', context)
+
+
+def contact(request):
+    context = {
+        'title': _('Contact Us')
+    }
+    return render(request, 'JourneyMap/contact_us.html', context)
