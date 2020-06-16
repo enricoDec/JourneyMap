0d7fb0a5c5b35e2250c0e935cadcffea3d9a1f65
enricoDec <enricodechadarevian@gmail.com>
2020-06-16 18:55:56 +0200

    Migrate to MySql

--- WebApplication/settings.py
+++ WebApplication/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import PasswordsDi
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'WebApplication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': PasswordsDi.DATABASES.get('ENGINE'),
+        'NAME': PasswordsDi.DATABASES.get('NAME'),
+        'USER': PasswordsDi.DATABASES.get('USER'),
+        'PASSWORD': PasswordsDi.DATABASES.get('PASSWORD'),
+        'HOST': PasswordsDi.DATABASES.get('HOST'),
+        'PORT': PasswordsDi.DATABASES.get('PORT'),
     }
 }
 
