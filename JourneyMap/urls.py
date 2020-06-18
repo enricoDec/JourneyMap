9ef74876b0373eaeffafa9f6e0132b92dbdc272b
enricoDec <enricodechadarevian@gmail.com>
2020-06-18 18:01:57 +0200

    rollback

--- JourneyMap/urls.py
+++ JourneyMap/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from .views import JourneyListView
 
 urlpatterns = [
     path('', views.home, name='JourneyMap_home'),
     path('contact_us/', views.contact, name='JourneyMap_contact_us'),
-    path('journeys/', JourneyListView.as_view(), name='JourneyMap_journeys')
-    # path('journeys/', views.journeys, name='JourneyMap_journeys'),
+    # path('journeys/', JourneyListView.as_view(), name='JourneyMap_journeys')
+    path('journeys/', views.journeys, name='JourneyMap_journeys'),
 ]
--- JourneyMap/views.py
+++ JourneyMap/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.utils.translation import gettext as _
-from django.views.generic import ListView
 from .models import Journey
-from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -19,17 +17,9 @@ def contact(request):
     return render(request, 'JourneyMap/contact_us.html', context)
 
 
-class JourneyListView(ListView):
-    model = Journey
-    template_name = 'JourneyMap/journeys.html'
-    context_object_name = 'journeys'
-    # order journeys by newest to oldest
-    ordering = ['-date_posted']
-
-# @login_required
-# def journeys(request):
-#     context = {
-#         'title': _('Journeys'),
-#         'journeys': Journey.objects.all()
-#     }
-#     return render(request, 'JourneyMap/journeys.html', context)
+def journeys(request):
+    context = {
+        'title': _('Journeys'),
+        'journeys': Journey.objects.all()
+    }
+    return render(request, 'JourneyMap/journeys.html', context)
