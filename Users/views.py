10d773d39287f79fcb21cc06bc10a97e6aabd680
enricoDec <enricodechadarevian@gmail.com>
2020-06-20 22:12:57 +0200

    add user change password

--- Users/views.py
+++ Users/views.py
@@ -1,8 +1,11 @@
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
 from django.utils.translation import gettext as _
-from django.contrib import messages
+
 from .forms import UserRegister, UserUpdateForm, ProfileUpdateForm
-from django.contrib.auth.decorators import login_required
 
 
 def sign_up(request):
@@ -24,24 +27,49 @@ def sign_up(request):
 
 
 @login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'Users/profile.html', {
+        'form': form
+    })
+
+
+@login_required
 def profile(request):
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST, instance=request.user)
         profile_form = ProfileUpdateForm(request.POST, request.FILES,
                                          instance=request.user.profile)
-        if user_form.is_valid() and profile_form.is_valid():
+        password_form = PasswordChangeForm(request.user, request.POST)
+        if user_form.is_valid() and profile_form.is_valid() and password_form.is_valid():
             user_form.save()
             profile_form.save()
+            user = password_form.save()
+            update_session_auth_hash(request, user)
             messages.success(request, _(f'Account information Updated!'))
             return redirect('profile')
-
+        else:
+            messages.error(request, 'Please correct the error below.')
     else:
+
         user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.profile)
+        password_form = PasswordChangeForm(request.user)
 
     context = {
         'title': _('Profile'),
         'user_form': user_form,
         'profile_form': profile_form,
+        'password_form': password_form,
     }
     return render(request, 'Users/profile.html', context)
