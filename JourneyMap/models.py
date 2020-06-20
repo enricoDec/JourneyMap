88c6be4e9cf4d06f8cbbff34bc36987fb106d44c
enricoDec <enricodechadarevian@gmail.com>
2020-06-20 20:49:54 +0200

    remove journey model

--- JourneyMap/models.py
+++ JourneyMap/models.py
@@ -1,14 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils import timezone
 
 
 class Journey(models.Model):
-    # id = models.ForeignKey()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    content = models.FileField()
-    date_posted = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return self.title
+    pass
