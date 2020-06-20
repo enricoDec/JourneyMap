e9983b066d94fc8cc296ac9e28a5b1a755f83aa9
enricoDec <enricodechadarevian@gmail.com>
2020-06-20 20:55:29 +0200

    add forgot your password (model, view, template)

--- WebApplication/settings.py
+++ WebApplication/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from config import mail
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -144,3 +146,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'JourneyMap_home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = mail.EMAIL_HOST
+EMAIL_HOST_USER = mail.EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = mail.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = mail.EMAIL_HOST_PASSWORD
+EMAIL_PORT = mail.EMAIL_PORT
+EMAIL_USE_TLS = mail.EMAIL_USE_TLS
--- WebApplication/urls.py
+++ WebApplication/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from Users import views as user_view
 from django.contrib.auth import views as auth_views
+from django.urls import path, include
+
+from Users import views as user_view
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
@@ -25,5 +26,16 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='Users/sign_in.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='Users/sign_out.html'), name='logout'),
     path('profile/', user_view.profile, name='profile'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='Users/password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='Users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='Users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='Users/password_reset_complete.html'),
+         name='password_reset_complete'),
     path('', include('JourneyMap.urls')),
 ]
