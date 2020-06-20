3e01e4ed26d8634539bfadf4903095fffc2142ad
enricoDec <enricodechadarevian@gmail.com>
2020-06-20 22:13:49 +0200

    delete old function

--- Users/views.py
+++ Users/views.py
@@ -27,24 +27,6 @@ def sign_up(request):
 
 
 @login_required
-def change_password(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user)  # Important!
-            messages.success(request, 'Your password was successfully updated!')
-            return redirect('change_password')
-        else:
-            messages.error(request, 'Please correct the error below.')
-    else:
-        form = PasswordChangeForm(request.user)
-    return render(request, 'Users/profile.html', {
-        'form': form
-    })
-
-
-@login_required
 def profile(request):
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST, instance=request.user)
