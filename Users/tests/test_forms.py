235129bb0b75d5473eadec8b5b9c59100a595bab
enricoDec <enricodechadarevian@gmail.com>
2020-06-30 19:32:00 +0200

    add tests, custom user manager, custom email template for registration

---
+++ Users/tests/test_forms.py
@@ -0,0 +1,76 @@
+from django.test import TestCase
+
+from Users.forms import UserRegister
+
+
+class TestForms(TestCase):
+
+    def test_register_form_valid_data(self):
+        form = UserRegister(data={
+            'first_name': 'Test',
+            'last_name': 'Test',
+            'email': 'test@journey-map.eu',
+            'username': 'testUser',
+            'password1': 'testing321',
+            'password2': 'testing321',
+        })
+
+        self.assertTrue(form.is_valid())
+
+    def test_register_form_nodata(self):
+        form = UserRegister(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 6)
+
+    def test_register_form_invalid_password(self):
+        form = UserRegister(data={
+            'first_name': 'Test',
+            'last_name': 'Test',
+            'email': 'test@journey-map.eu',
+            'username': 'testUser',
+            'password1': 'testing321',
+            'password2': 'testingNOT321',
+        })
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
+
+    def test_register_form_simple_password(self):
+        form = UserRegister(data={
+            'first_name': 'Test',
+            'last_name': 'Test',
+            'email': 'test@journey-map.eu',
+            'username': 'testUser',
+            'password1': 'password',
+            'password2': 'password',
+        })
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
+
+    def test_register_form_password_similar_to_name(self):
+        form = UserRegister(data={
+            'first_name': 'MyName',
+            'last_name': 'MyName',
+            'email': 'test@journey-map.eu',
+            'username': 'testUser',
+            'password1': 'MyName98',
+            'password2': 'MyName98',
+        })
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
+
+    def test_register_form_email_invalid(self):
+        form = UserRegister(data={
+             'first_name': 'Test',
+            'last_name': 'Test',
+            'email': 'testjourney-map.eu',
+            'username': 'testUser',
+            'password1': 'testing321',
+            'password2': 'testing321',
+        })
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
--- Users/views.py
+++ Users/views.py
@@ -2,18 +2,21 @@ from django.contrib import messages
 from django.contrib.auth import get_user_model, login
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
-from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.translation import gettext as _
 from django.views import View
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
 
 from .forms import UserRegister
 from .tokens import account_activation_token
 
 User = get_user_model()
+
 
 def sign_in(request):
     context = {
@@ -22,6 +25,8 @@ def sign_in(request):
     return render(request, 'Users/sign_in.html', context)
 
 
+@never_cache
+@csrf_protect
 def sign_up(request):
     if request.method == 'POST':
         form = UserRegister(request.POST)
@@ -31,16 +36,27 @@ def sign_up(request):
             user.save()
             form.save()
 
-            # Send an email
-            mail_subject = 'Activate your account.'
+            # One time token to confirm email
             current_site = get_current_site(request)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
             token = account_activation_token.make_token(user)
-            activation_link = "{0}/activate/{1}/{2}".format(current_site, uid, token)
-            message = "Hello {0},\n {1}".format(user.username, activation_link)
+            activation_link = "http://{0}/activate/{1}/{2}".format(current_site, uid, token)
+
+            context_email = {
+                'subject': 'Activate your account',
+                'link': activation_link,
+                'name': user.first_name + ' ' + user.last_name,
+            }
+
+            # Send an email with html template
             to_email = form.cleaned_data.get('email')
-            email = EmailMessage(mail_subject, message, to=[to_email])
-            email.send()
+            html_email = 'Users/sign_up_email.html'
+            html_message = render_to_string(html_email, context_email)
+
+            message = EmailMessage(_('Activate your account'), html_message, 'auth@journey-map.eu', [to_email])
+            message.content_subtype = 'html'
+            message.send()
+
             messages.success(request, _('Please confirm your email address to complete the registration'))
             return redirect("JourneyMap_home")
     else:
@@ -66,13 +82,8 @@ class ActivateUser(View):
             user.save()
             login(request, user)
 
+            messages.success(request, _('Activation successful!'))
             return redirect('login')
         else:
-            return HttpResponse('Activation link is invalid!')
-
-    # def post(self, request):
-    #     form = PasswordChangeForm(request.user, request.POST)
-    #     if form.is_valid():
-    #         user = form.save()
-    #         update_session_auth_hash(request, user)
-    #         return HttpResponse('Password changed successfully')
+            messages.warning(request, _('Activation link is invalid!'))
+            return redirect('JourneyMap_home')
