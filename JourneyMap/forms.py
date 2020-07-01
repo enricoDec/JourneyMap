9ac1c1f34351002a40bc92b28aa33fa4af4e86a5
enricoDec <enricodechadarevian@gmail.com>
2020-07-01 17:13:09 +0200

    changed "hard-coded emails" to email templates user and internal email are now send add translations for contact form

--- JourneyMap/forms.py
+++ JourneyMap/forms.py
@@ -5,4 +5,4 @@ from django.utils.translation import ugettext_lazy as _
 class ContactForm(forms.Form):
     name = forms.CharField(required=True, label=_(u'Name'), max_length=100)
     email = forms.EmailField(required=True, label=_(u'Email'), max_length=100)
-    message = forms.CharField(required=True, label=_(u'Message'), widget=forms.Textarea, max_length=1000)
+    message = forms.CharField(required=True, label=_(u'Message'), widget=forms.Textarea, max_length=1000, min_length=12)
--- JourneyMap/urls.py
+++ JourneyMap/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='JourneyMap_home'),
     path('contact_us/', views.contact, name='JourneyMap_contact_us'),
-    path('contact_us/thanks', views.contact_thanks, name='JourneyMap_contact_us_thanks'),
 ]
